23d8bfc8e9bd159a4f1f2f8768c0935650f83f73
Victor Siquetto <victor.siquetto@gmail.com>
2026-01-19 21:50:41 -0300

    exercicio finalizado

---
+++ Exercicios-Python/Mundo 2/ex060.py
@@ -0,0 +1,11 @@
+# Exercício 60 – Cálculo do Fatorial
+
+num = int(input('Digite um numero para saber seu fatorial: '))
+c = num
+f = 1
+while c > 0:
+    print(c, end ='')
+    print(' x ' if c > 1 else ' = ', end ='')
+    f *= c
+    c -= 1
+print(f)    
