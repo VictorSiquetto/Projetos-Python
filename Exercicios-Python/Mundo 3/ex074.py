7d31120798e32dbe1b4982ec3674f436fc496df3
Victor Siquetto <victor.siquetto@gmail.com>
2026-01-27 20:33:42 -0300

    exercicio finalizado

--- Exercicios-Python/Mundo 3/ex074.py
+++ Exercicios-Python/Mundo 3/ex074.py
@@ -1,2 +1,19 @@
-# 
+# Exercício 74 – Maior e menor valores em Tupla
 
+from random import randint
+
+numeros = (randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10),)
+print('Os valores sorteados foram: ', end ='')
+for c in numeros:
+    print(c, end =' ')
+print(f'\nO maior numero foi {max(numeros)}')
+print(f'O menor numeto foi {min(numeros)}')
+
+# from random import sample     para os numeros nao se repetirem
+
+# numeros = sample(range(1, 11), 5)    
+# print('Os valores sorteados foram: ', end ='')
+# for c in numeros:
+#     print(c, end =' ')
+# print(f'\nO maior numero foi {max(numeros)}')
+# print(f'O menor numeto foi {min(numeros)}')
