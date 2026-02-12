3c9b208f6277334b82f2269fada4c7eb1a72b09a
Victor Siquetto <victor.siquetto@gmail.com>
2026-02-11 22:16:17 -0300

    exercicio finalizado

--- Exercicios-Python/Mundo 3/ex094.py
+++ Exercicios-Python/Mundo 3/ex094.py
@@ -5,6 +5,7 @@ lista = []
 mulheres = []
 tot = 0
 while True:
+    dic.clear()
     dic['nome'] = str(input('Nome: '))
     dic['sexo'] = ' '
     while dic['sexo'] not in 'MF':
@@ -35,3 +36,41 @@ for c in lista:
     if c['idade'] > media:
         print(f"Nome = {c['nome']}; Sexo = {c['sexo']}; Idade = {c['idade']}")
 print('Encerrado')
+
+# dic = {}
+# lista = []
+# tot = 0
+# while True:
+#     dic.clear()
+#     dic['nome'] = str(input('Nome: '))
+#     while True:
+#         dic['sexo'] = str(input('Sexo [M/F]: ')).strip().upper()[0]
+#         if dic['sexo'] in 'MF':
+#             break
+#         print('Erro, digite apenas M ou F')
+#     dic['idade'] = int(input('Idade: '))
+#     tot += dic['idade']
+#     lista.append(dic.copy())
+#     while True:
+#         opcao = str(input('Quer continuar [S/N]: ')).strip().upper()[0]
+#         if opcao in 'SN':
+#             break
+#         print('Erro, digite apenas S ou N')
+#     if opcao == 'N':
+#         break
+# media = tot / (len(lista))
+# print('=-'*20)
+# print(f'Ao todo temos {len(lista)} pessoas cadastradas')
+# print(f'A media de idade é de {media:.2f} anos')
+# print('As mulheres cadastradas foram', end =' ')
+# for m in lista:
+#     if m['sexo'] in 'Ff':
+#         print(m['nome'], end =' ')
+# print()
+# print('Lista das pessoas que estao acima da media: ')
+# for c in lista:
+#     if c['idade'] > media:
+#         for k, v in c.items():
+#             print(f'{k} = {v}; ', end = '')
+#         print()
+# print('Encerrado')
