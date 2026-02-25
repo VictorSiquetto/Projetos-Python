a8be036e16a20fb6aa7df5764ebd8e8d690be8da
Victor Siquetto <victor.siquetto@gmail.com>
2026-02-24 23:57:42 -0300

    exercicio finalizado

---
+++ Exercicios-Python/Mundo 3/ex103.py
@@ -0,0 +1,15 @@
+# Exercício 103 – Ficha do Jogador
+
+def ficha(j = '<desconhecido>', g = 0):
+    print(f'O jogador {j} fez {g} gol(s) no campeonato')
+
+j = str(input('Nome do jogador: '))
+g = str(input('Numero de gols: '))
+if g.isnumeric():
+    g = int(g)
+else:
+    g = 0
+if j.strip() == '':
+    ficha(g = g)
+else:
+    ficha(j, g)
