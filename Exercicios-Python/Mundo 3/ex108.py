3deba888b3ea72d63aa44bd6de2f5e7f784ff295
Victor Siquetto <victor.siquetto@gmail.com>
2026-02-25 22:33:20 -0300

    exercicio finalizado

---
+++ Exercicios-Python/Mundo 3/ex108.py
@@ -0,0 +1,9 @@
+# Exercício 108 – Formatando Moedas em Python
+
+from modulos import moeda
+
+p = float(input('Digite o preço: R$'))
+print(f'O dobro de {moeda.moeda(p)} é {moeda.moeda(moeda.dobro(p))}')
+print(f'A metade de {moeda.moeda(p)} é {moeda.moeda(moeda.metade(p))}')
+print(f'Aumentando 13% de {moeda.moeda(p)} é {moeda.moeda(moeda.aumentar(p, 13))}')
+print(f'Diminuindo 15% de {moeda.moeda(p)} é {moeda.moeda(moeda.diminuir(p, 15))}')
--- Exercicios-Python/Mundo 3/modulos/moeda.py
+++ Exercicios-Python/Mundo 3/modulos/moeda.py
@@ -1,15 +1,18 @@
-def dobro(num):
+def dobro(num = 0):
     n = num * 2
     return n
 
-def metade(num):
+def metade(num = 0):
     n = num / 2
     return n
 
-def aumentar(num, p):
+def aumentar(num = 0, p = 0):
     n = num + (num * p / 100)
     return n
 
-def diminuir(num, p):
+def diminuir(num = 0, p = 0):
     n = num - (num * p / 100)
     return n
+
+def moeda(num = 0, moeda = 'R$'):
+    return f'{moeda}{num:.2f}'.replace('.', ',')
