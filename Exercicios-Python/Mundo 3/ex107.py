6c8cb0826dc7d21e1108d95d795c08aa979b98ab
Victor Siquetto <victor.siquetto@gmail.com>
2026-02-25 22:25:02 -0300

    exercicio finalizado

--- Exercicios-Python/Mundo 3/ex107.py
+++ Exercicios-Python/Mundo 3/ex107.py
@@ -3,7 +3,7 @@
 from modulos import moeda
 
 p = float(input('Digite o preço: R$'))
-print(f'O dobro de {p} é {moeda.dobro(p):.2f}')
-print(f'A metade de {p} é {moeda.metade(p):.2f}')
-print(f'Aumentando 13% de {p} é {moeda.aumentar(p, 13):.2f}')
-print(f'Diminuindo 15% de {p} é {moeda.diminuir(p, 15):.2f}')
+print(f'O dobro de {p} é {moeda.dobro(p)}')
+print(f'A metade de {p} é {moeda.metade(p)}')
+print(f'Aumentando 13% de {p} é {moeda.aumentar(p, 13)}')
+print(f'Diminuindo 15% de {p} é {moeda.diminuir(p, 15)}')
