f61be6d0900a70abdafe9a5a8eb03966a79e25cd
Victor Siquetto <victor.siquetto@gmail.com>
2026-03-12 23:24:47 -0300

    exercicio finalizado

--- Exercicios-Python/Mundo 4/ex001/ex001.py
+++ Exercicios-Python/Mundo 4/ex001/ex001.py
@@ -2,13 +2,15 @@ from rich import *
 
 class Funcionario:
 
+    empresa = 'Curso em Video'
+
     def __init__(self, nome, setor, cargo):
         self.nome = nome
         self.setor = setor
         self.cargo = cargo
     
-    def apresentar(self):
-        return f':waving_hand: Ola, sou [blue]{self.nome}[/] e sou {self.cargo} do setor de {self.setor}'
+    def apresentar(self) -> str:
+        return f':waving_hand: Ola, sou [blue]{self.nome}[/] e sou {self.cargo} do setor de {self.setor} da empresa {Funcionario.empresa}'  #{self.__class__.empresa}
     
 c1 = Funcionario('Maria', 'Administracao', 'Diretora')
 print(c1.apresentar())
---
+++ Exercicios-Python/Mundo 4/ex002/ex002.py
@@ -0,0 +1,21 @@
+from rich import *
+from rich.panel import Panel
+
+class Produto:
+
+    def __init__(self, nome, preco):
+        self.nome = nome
+        self.preco = preco
+    
+    def etiqueta(self):
+        conteudo = f'{self.nome.center(30, ' ')}'
+        conteudo += f'{'-'*30}'
+        precof = f'R${self.preco:.2f}'
+        conteudo += f'{precof.center(30, '.')}'
+        etiqueta = Panel(conteudo, title='Produto', width=34)
+        print(etiqueta)
+
+p1 = Produto('Iphone 17', 12000)
+p1.etiqueta()
+p2 = Produto('Notebook Gamer', 4500)
+p2.etiqueta()
